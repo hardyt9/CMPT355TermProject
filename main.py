93f4b37535b34dc933e66717e6abe0369aa675ca
bwfunk <58319076+bwfunk@users.noreply.github.com>
2024-02-21 23:13:23 -0700

    Created konane playing board class

--- main.py
+++ main.py
@@ -1,6 +1,31 @@
 # This file will contains all the implementation of the minimax algorithm and aplha-beta pruning. (subject to change)
 
+# Implements the minimax algorithm
 class KonaneAI:
     def __init__(self):
+        pass
 
-        
+# Implements the playing board - 8x8, alternating black and white tiles
+class KonaneBoard:
+    def __init__(self):
+        self.board = []
+        self.create_board()
+
+    def create_board(self):
+        for i in range(8):
+            if i % 2 == 0:
+                self.board.append(self.create_row('B','W'))
+            else:
+                self.board.append(self.create_row('W','B'))
+        for row in self.board:
+            print(row)
+
+    def create_row(self, color1, color2):
+        row = []
+        for i in range(8):
+            if i%2 == 0:
+                row.append(color1)
+            else:
+                row.append(color2)
+        return row
+
